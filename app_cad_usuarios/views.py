8e74ef721d55d45b158111cda2d6c93f3d659a3d
jGean <josegeantlc@gmail.com>
2024-06-27 22:43:11 -0300

    atualizar_melhor

--- app_cad_usuarios/views.py
+++ app_cad_usuarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from app_cad_usuarios import views
+from .models import Usuario
 
 # Create your views here.
 
@@ -7,4 +8,14 @@ def home(request):
     return render(request, 'usuarios/home.html')
 
 def usuarios(request):
-    pass
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+    
+    #exibir todo em nova da pasta
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+    #retorna os usuarios pra pagina
+    return render(request,'usuarios/usuarios.html',usuarios)
